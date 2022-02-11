bdf05f4a2926a37a0d58c78ff71f7902a417a06d
Sruthi <93090631+ChinthimiSruthi@users.noreply.github.com>
2022-02-11 10:46:00 +0000

    p4final

---
+++ p3final.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int input_number()
+{
+  int n;
+  printf("Enter size\n");
+  scanf("%d",&n);
+  return 0;
+}
+int is_composite(int n)
+{
+  
+}
+void output(int n,int composite)
+{
+  printf("%d is a composite number\n",composite);
+}
+int main()
+{
+  int a,n,composite;
+  input_number();
+  is_composite(n);
+  output(n,composite);
+  return 0;
+}
