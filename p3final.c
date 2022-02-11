906c61b9706fe86edafef9bdde3878ea8e44b627
Sruthi <93090631+ChinthimiSruthi@users.noreply.github.com>
2022-02-11 13:53:10 +0000

    p3final

--- p3final.c
+++ p3final.c
@@ -1,24 +1,38 @@
-#include <stdio.h>
+#include<stdio.h>
+#include<math.h>
 int input_number()
 {
   int n;
-  printf("Enter size\n");
-  scanf("%d",&n);
-  return 0;
+  printf("enter the number\n");
+scanf("%d",&n);
+return n;
 }
 int is_composite(int n)
 {
-  
+  int i=2;
+  while(i<=n/2)
+  {
+    if(n%i==0)
+     return 1;
+     i++;
+  }
+  return 0;
 }
-void output(int n,int composite)
+void output(int n,int is_composite)
 {
-  printf("%d is a composite number\n",composite);
+  if(is_composite)
+  {
+    printf("The number %d is composite\n",n);
+  }
+  else
+  {
+    printf("The number is not composite\n");
+  }
 }
 int main()
 {
-  int a,n,composite;
-  input_number();
-  is_composite(n);
-  output(n,composite);
-  return 0;
+ int n=input_number();
+ int composite= is_composite(n);
+ output(n,composite);
+ return 0;
 }
